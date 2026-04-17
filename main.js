fe46d72c07b4bdb106806e75ef64b0fa916a853c
Harish <rielerop@gmail.com>
2026-04-17 22:54:13 +0545

    Comments + updates

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 let id =1;
+
+// Toggle "Add" button on "Enter" keypress
+const input = document.getElementById('textBox');
+const enterBtn = document.getElementById('bt1');
+input.addEventListener("keydown", function(event){
+    if(event.key==="Enter"){
+        enterBtn.click();
+    }
+})
+
+// Input text from user and pass it to createPlan()
 function add(){
-    const plan = document.getElementById('text1').value;
+    const plan = document.getElementById('textBox').value;
     if(plan!=''){
         createPlan(plan,id);
         id++;
@@ -10,28 +21,35 @@ function add(){
     }
 }
 
-
+// Create plan
 function createPlan(plan,id){
+    // Create checkbox
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.id = 'cb' + id;
 
+    // Create checkbox text (label)
     const checkboxText = document.createElement('label');
     checkboxText.id = 'cbText' + id;
+    checkboxText.contentEditable = "true";
+    checkboxText.style.cursor = "pointer";
     checkboxText.append(document.createTextNode(plan));
 
+    // Append checkbox and checkbox text to planContainer(div)
     const cbDiv = document.getElementById("planContainer");
     const br = document.createElement("br");
 
     cbDiv.append(checkbox);
     cbDiv.append(checkboxText);
     cbDiv.append(br)
-    sysMsg.value('');
+    
+    // Extra
+    document.getElementById("textBox").value='';
     window.alert("Added plan" + "" + id);
     id++;
 }
 
 function clearPlans(){
     document.getElementById("planContainer").innerHTML = "";
-    document.getElementById("text1").value='';
+    document.getElementById("textBox").value='';
 }
